799f1247d13105a5dce51b782306dbe08539966f
Stephan Meijer <stephan.meijer@gmail.com>
2022-07-02 13:42:58 +0200

    chore: disable storybook shortcuts

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,12 @@
+import { addons } from '@storybook/addons';
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
 };
+
+// Some stories may set up keyboard event handlers, which interfers with storybook
+addons.setConfig({
+  enableShortcuts: false,
+});
